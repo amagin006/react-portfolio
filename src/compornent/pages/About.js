35d4f5974338b9f9b9a2867a57b764ab5e5d6dc2
Shota Iwamoto <amaginqoo@gmail.com>
2019-08-25 02:35:17 -0700

    Modify About animation

--- src/compornent/pages/About.js
+++ src/compornent/pages/About.js
@@ -4,38 +4,68 @@ import Delighters from '../common/delighters'
 
 class About extends React.Component {
 
+  constructor(props) {
+    super(props)
+    const body = document.querySelector('body');
+    body.className += ' noSidebar'
+  }
+
   componentDidMount() {
     Delighters.init();
+    window.addEventListener('scroll', this.listenToScroll)
+    window.addEventListener('mousewheel', this.listenToScroll)
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.listenToScroll)
+    window.removeEventListener('mousewheel', this.listenToScroll)
   }
     
   render() {
     return (
       <>
         <div className="who-sec">
-          <div className="w-section1">
-            <div className="bg-1 left" data-delighter></div>
+          <div className="a-section1" data-delighter>
+            <div className="section-inner">
+              <div className="bg-1 left" data-delighter></div>
+            </div>
           </div> 
 
-          <div className="w-section2">
+          <div className="a-section2">
             <div className="wrapper">
               <div className="photo_1" data-delighter>
                 <img src={aboutImages.find(image => image.itemNum === 1).url} alt=""/>
               </div>
             </div>
           </div>
-          <div className="w-section3" data-delighter>
+          <div className="a-section3" data-delighter>
             <div className="bg-3" data-delighter>
               <div className="bg-3-c bg-3-1"></div>
               <div className="bg-3-c bg-3-2"></div>
               <div className="bg-3-c bg-3-3"></div>
               <div className="bg-3-c bg-3-4"></div>
               <div className="bg-3-c bg-3-5"></div>
-              <div className="skill-box">
-                <h3 className="skill-t line-1">Skills</h3>
-                <div className="skill-t line-2">iOS / Swift / Objective-C / Java / Android</div>
-                <div className="skill-t line-3">HTML5 / CSS3 / JavaScript / React / jQuery</div>
-                <div className="skill-t line-4">WordPress / SASS / Webpack / Gulp</div>
-                <div className="skill-t line-5">Git / Bootstrap / AdobeXD / ...and more</div>
+              <div className="a-section3-box fl fl-bt">
+                <div className="skill-box fl fl-column  fl-ju-ce">
+                  <h3 className="skill-t title line-1">Skills</h3>
+                  <div className="skill-t line-2">iOS / Swift / Objective-C / Java / Android</div>
+                  <div className="skill-t line-3">JavaScript / React / Redux / HTML5 / CSS3 </div>
+                  <div className="skill-t line-4">WordPress /  jQuery / SASS / Webpack / Gulp</div>
+                  <div className="skill-t line-5">Git / pug / Bootstrap / AdobeXD / ...and more</div>
+                  <div className="skill-t line-6"></div>
+                  <div className="skill-disc">
+                    <div className="disc-title">What I Aim</div>
+                    <p>My Goal is not only builting web and app, also understanding businesses and creating good user expriences.</p>
+
+                  </div>
+
+                <div className="ref-box">
+                  <a href="https://www.linkedin.com/in/shota-iwamoto001/">
+                    linkedin
+                  </a>
+                </div>
+                </div>
+                <div className="desc-box"></div>
               </div>
             </div>
             <div className="who-text-block" data-delighter>
@@ -46,7 +76,6 @@ class About extends React.Component {
               <p>I’ve worked as a freelance remotely for agencies, collaborated with people for businesses.  I always eager to learn more and communicate with people.</p>
             </div>
           </div>
-          
         </div>
       </>
     );
