24e0f80b63e087f4b522eeac524c5563a8d49963
Shota Iwamoto <amaginqoo@gmail.com>
2019-08-28 00:17:56 -0700

    Add animation workpage

--- src/compornent/pages/Works.js
+++ src/compornent/pages/Works.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import WorkImgBox from '../WorkImgBox';
+import { Route, Switch, Link } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from "react-transition-group"
 
 import { workContents } from  '../../constants/workContentInfo';
+import Works1 from '../WorkItem/Work1'
+import Works2 from '../WorkItem/Work2';
+import Works3 from '../WorkItem/Work3';
+import Page404 from '../pages/Page404';
 
 class Work extends React.Component {
 
@@ -32,12 +37,39 @@ class Work extends React.Component {
     const work = workContents.filter(work => {
       return this.state.visibleItem === work.itemNum
     })
-
+    const { url, imgUrl, title, disc, text } = work[0]
     return (
       <div className="work-content">
         <div className="work-item-wrapper">
           <div className="work-box">
-            <WorkImgBox work={work[0]} location={this.props.location} />
+            <Route render={({ location }) =>  (
+              <TransitionGroup>
+              <CSSTransition
+                      key={this.props.location.key}
+                      classNames="fade"
+                      timeout={300}
+                    >
+                <Switch location={location}>
+                  <Route exact path='/works'>
+                    <Link to={`${this.props.location.pathname}${url}`}>
+                      <div className="work-item-box">
+                        <img src={imgUrl} alt=""/>
+                        <div className="w-title-box">
+                          <div className="title">{title}</div>
+                          <div className="disc">{disc}</div>
+                          <div className="text">{text}</div>
+                        </div>
+                      </div>
+                    </Link>
+                  </Route>
+                  <Route exact path="/works/work1" component={Works1}/>
+                  <Route exact path="/works/work2" component={Works2}/>
+                  <Route exact path="/works/work3" component={Works3}/> 
+                  <Route component={Page404} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+            )} />
           </div>
         </div>
       </div>
