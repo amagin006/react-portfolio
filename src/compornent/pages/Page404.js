60b577bc8e716edb0084af5f0072b5c1704d9ca1
Shota Iwamoto <amaginqoo@gmail.com>
2019-09-06 00:03:13 -0700

    Add 404 page

--- src/compornent/pages/Page404.js
+++ src/compornent/pages/Page404.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Page404 extends React.Component {
   render() {
-    return <div>This is 404 page</div>
+    return (
+      <div className="notFound-page">
+        <div className="notFound-bg">
+          <div className="text-wrapper">
+            <h2>404</h2>
+            <p>Sorry...</p>
+            <p>This is not the page you are looking for.</p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
