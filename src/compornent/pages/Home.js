d10135ac66d0ccd07a7d36bb31f5a82bf696f5e3
Shota Iwamoto <amaginqoo@gmail.com>
2019-09-21 23:59:35 -0700

    Change

--- src/compornent/pages/Home.js
+++ src/compornent/pages/Home.js
@@ -8,7 +8,8 @@ class Home extends React.Component {
         <div className="main-visual">
           <div className="main-visual-wrapper">
             <div className="main-visual-title-block">
-              <div className="title-top">Web & Mobile Developer</div>
+              <div className="title-top">React Web Developer</div>
+              <div className="title-top">iOS Devoloper</div>
               <h1>SHOTA <br className="lg-none"/>IWAMOTO</h1>
             </div>
           </div>
